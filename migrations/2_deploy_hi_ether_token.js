1b6f532bd46786a74bb55edf82f9aacf6f3140df
Kentaro Kuribayashi <kentarok@gmail.com>
2017-12-05 15:32:53 +0900

    add migration script to ropsten test net

--- migrations/2_deploy_hi_ether_token.js
+++ migrations/2_deploy_hi_ether_token.js
@@ -2,5 +2,7 @@ const HiEtherToken = artifacts.require('./HiEtherToken.sol')
 
 module.exports = (deployer) => {
   let initialSupply = 50000e18
-  deployer.deploy(HiEtherToken, initialSupply)
+  deployer.deploy(HiEtherToken, initialSupply, {
+      gas: 2000000
+  })
 }
--- truffle.js
+++ truffle.js
@@ -1,4 +1,18 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env.ROPSTEN_MNEMONIC;
+var accessToken = process.env.INFURA_ACCESS_TOKEN;
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          "https://ropsten.infura.io/" + accessToken
+        );
+      },
+      network_id: 3,
+      gas: 500000
+    }   
+  }
 };
